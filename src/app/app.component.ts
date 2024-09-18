efcc26669ff26394550f28a6ff46293941691856
JanhaviPawar1810 <70591560+JanhaviPawar1810@users.noreply.github.com>
2024-09-18 17:50:14 +0100

    Updated production files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'personal-website';
+  title = 'myPortfolio';
   isSubmenuVisible: boolean = false;
 
   toggleSubmenu() {
